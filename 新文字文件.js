7108ae551b3103276e58df2d8e4f8d298a28f8d9
HayateSwat <hayateswat@gmail.com>
2013-07-10 14:06:52 +0800

    Convert to JS
    Title

---
+++ 新文字文件.js
@@ -0,0 +1,44 @@
+var geocoder;
+var map;
+function initialize() {
+  geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng( 23.7162184,120.4242401);
+  var mapOptions = {
+    zoom: 11,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  showMarker();
+	
+}
+
+var session = new Array();
+
+function showMarker(){			
+	var infowindow = new google.maps.InfoWindow();
+	var tempIcon;
+	var mousemovein = 
+	function(event) {
+		for(var j in session){
+			if(event.latLng.equals(session[j].marker.getPosition())){
+				i=j;
+				infowindow.setContent(session[i].content+event.latLng.toString());
+				infowindow.open(map,session[i].marker);
+				tempIcon = session[i].marker.getIcon();
+				break;
+			}
+		}
+	}
+	
+	for(var i in session){
+		google.maps.event.addListener(session[i].marker,'mouseover', mousemovein);
+		google.maps.event.addListener(session[i].marker,'mouseout', function() {
+		infowindow.close();
+		session[i].marker.setIcon(tempIcon);
+		});
+	}
+	
+}
+google.maps.event.addDomListener(window, 'load', initialize);
